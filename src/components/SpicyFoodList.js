ee9c1653bc9563552adfb3545fc4a16aa4c38211
Jeremiah <jsnidar@gmail.com>
2021-09-03 21:27:10 -0500

    commit tree d5af6d469a0ece6dfb229c4aedd488cfa4dc625a

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,16 +3,64 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] =useState('All')
+  
+  const foodsToDisplay = foods.filter((food) => {
+    if(filterBy === 'All') {
+      return true;
+    }else{
+      return food.cuisine === filterBy;
+    }
+  });
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray);
   }
 
+  function handleClick (id) {
+    //My solution
+    const newFoodArray = [...foods]
+    const index = newFoodArray.findIndex(food => food.id === id)
+    newFoodArray[index].heatLevel++
+    //Their solution. 
+    // const newFoodArray = foods.map((food) => {
+    //   if (food.id === id) {
+    //     return {
+    //       ...food,
+    //       heatLevel: food.heatLevel + 1,
+    //     };
+    //   } else {
+    //     return food;
+    //   }
+    // });
+    //I don't know which is better. Mine covers less lines. At first I thought I liked theirs better but now I don't know. 
+    setFoods(newFoodArray)
+  }
+  
+  const list = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleClick (food.id)}>
+      { food.name }| Cuisine: { food.cuisine } | Heat: { food.heatLevel}
+    </li> 
+  ))
+  
+  function handleDropdownFilter (event){
+    setFilterBy(event.target.value)
+    console.log(foods)
+  }
+ 
   return (
     <div>
+      <select onChange={handleDropdownFilter}name="filter">
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{list}</ul>
     </div>
   );
 }
